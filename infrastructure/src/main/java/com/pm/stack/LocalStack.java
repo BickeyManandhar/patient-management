3304af055ee14801033afca15b2e9ab4cf29a50b
BickeyManandhar <manandharbickey@gmail.com>
2025-09-15 17:11:03 -0400

    BICKEY-09152025 creating api-gateway service that has ALB

--- infrastructure/src/main/java/com/pm/stack/LocalStack.java
+++ infrastructure/src/main/java/com/pm/stack/LocalStack.java
@@ -5,6 +5,7 @@ import software.amazon.awscdk.services.ec2.*;
 import software.amazon.awscdk.services.ec2.InstanceType;
 import software.amazon.awscdk.services.ecs.*;
 import software.amazon.awscdk.services.ecs.Protocol;
+import software.amazon.awscdk.services.ecs.patterns.ApplicationLoadBalancedFargateService;
 import software.amazon.awscdk.services.logs.LogGroup;
 import software.amazon.awscdk.services.logs.RetentionDays;
 import software.amazon.awscdk.services.msk.CfnCluster;
@@ -49,7 +50,7 @@ public class LocalStack extends Stack {
         authService.getNode().addDependency(authServiceDb);
 
         FargateService billingService = createFargateService("BillingService",
-                "nilling-service",
+                "billing-service",
                 List.of(4001, 9001),
                 null,
                 null);
@@ -63,7 +64,7 @@ public class LocalStack extends Stack {
         analyticsService.getNode().addDependency(mskCluster); //it has dependency on kafka
 
         FargateService patientService = createFargateService("PatientService",
-                "patiebt-service",
+                "patient-service",
                 List.of(4000),
                 patientServiceDb,
                 Map.of(
@@ -73,6 +74,8 @@ public class LocalStack extends Stack {
         patientService.getNode().addDependency(patientDbHealthCheck);
         patientService.getNode().addDependency(billingService);
         patientService.getNode().addDependency(mskCluster);
+
+        createApiGatewayService();
     }
 
     //Step 1: Creating VPC
@@ -152,7 +155,7 @@ public class LocalStack extends Stack {
                 .build();
     }
 
-    //Step 6: Creating ECS Fargate Service
+    //Step 6: Creating ECS Fargate Services excluding api-gateway
     private FargateService createFargateService(
             String id,
             String imageName,
@@ -217,6 +220,51 @@ public class LocalStack extends Stack {
                 .assignPublicIp(false)
                 .serviceName(imageName)
                 .build();
+    }
+
+    //Step 7: Creating api-gateway service with ALB
+    private void createApiGatewayService(){
+        FargateTaskDefinition taskDefinition = FargateTaskDefinition.Builder.create(this, "APIGatewayTaskDefination")
+                .cpu(256)
+                .memoryLimitMiB(512)
+                .build();
+
+        ContainerDefinitionOptions containerOptions =
+                ContainerDefinitionOptions.builder()
+                        .image(ContainerImage.fromRegistry("api-gateway"))
+                        .environment(Map.of(
+                                "SPRING_PROFILE_ACTIVE", "prod",
+                                "AUTH_SERVICE_URL","http://host.docker.internal:4005"
+                        ))
+                        .portMappings(List.of(4004).stream()
+                                .map(port -> PortMapping.builder()
+                                        .containerPort(port)
+                                        .hostPort(port)
+                                        .protocol(Protocol.TCP)
+                                        .build())
+                                .toList())
+                        .logging(LogDriver.awsLogs(AwsLogDriverProps.builder()
+                                .logGroup(LogGroup.Builder.create(this, "ApiGatewayLogGroup")
+                                        .logGroupName("/ecs/api-gateway")
+                                        .removalPolicy(RemovalPolicy.DESTROY)
+                                        .retention(RetentionDays.ONE_DAY)
+                                        .build())
+                                .streamPrefix("api-gateway")
+                                .build()))
+                        .build();
+
+        taskDefinition.addContainer("APIGatewayContainer", containerOptions);
+
+        //this will automatically create ALB
+        ApplicationLoadBalancedFargateService apiGateway
+                = ApplicationLoadBalancedFargateService.Builder.create(this,"APIGatewayService")
+                .cluster(ecsCluster)
+                .serviceName("api-gateway")
+                .taskDefinition(taskDefinition)
+                .desiredCount(1)
+                .healthCheckGracePeriod(Duration.seconds(60))
+                .build();
+
     }
 
     public static void main(final String[] args) {
