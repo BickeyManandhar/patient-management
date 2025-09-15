1c4d58214fea64e20fccb9a82111714d0c436d6e
BickeyManandhar <manandharbickey@gmail.com>
2025-09-15 15:23:59 -0400

    BICKEY-09152025 creating DB and adding health check

--- infrastructure/src/main/java/com/pm/stack/LocalStack.java
+++ infrastructure/src/main/java/com/pm/stack/LocalStack.java
@@ -1,7 +1,12 @@
 package com.pm.stack;
 
 import software.amazon.awscdk.*;
+import software.amazon.awscdk.services.ec2.InstanceClass;
+import software.amazon.awscdk.services.ec2.InstanceSize;
+import software.amazon.awscdk.services.ec2.InstanceType;
 import software.amazon.awscdk.services.ec2.Vpc;
+import software.amazon.awscdk.services.rds.*;
+import software.amazon.awscdk.services.route53.CfnHealthCheck;
 
 public class LocalStack extends Stack {
 
@@ -11,12 +16,51 @@ public class LocalStack extends Stack {
         super(scope, id, props);
 
         this.vpc = createVpc();
+
+        DatabaseInstance authServiceDb = createDatabase("AuthServiceDB", "auth-service-db");
+
+        DatabaseInstance patientServiceDb = createDatabase("PatientServiceDB", "patient-service-db");
+
+        CfnHealthCheck authDbHealthCheck = createDbHealthCheck(authServiceDb, "AuthServiceDBHealthCheck");
+
+        CfnHealthCheck patientDbHealthCheck = createDbHealthCheck(patientServiceDb, "PatientServiceDBHealthCheck");
     }
 
     //Step 1: Creating VPC
     private Vpc createVpc() {
         return Vpc.Builder.create(this, "PatientManagementVPC").vpcName("PatientManagementVPC")
                 .maxAzs(2)
+                .build();
+    }
+
+    //Step 2: Creating DB instance
+    private DatabaseInstance createDatabase(String id, String dbName){
+        return DatabaseInstance.Builder
+                .create(this, id)
+                .engine(DatabaseInstanceEngine
+                        .postgres(PostgresInstanceEngineProps.builder()
+                        .version(PostgresEngineVersion.VER_17_2).build()))
+                .vpc(vpc) //connecting our DB to vpc created above
+                .instanceType(InstanceType.of(InstanceClass.BURSTABLE2, InstanceSize.MICRO)) //compute levels; doing minimum for local
+                .allocatedStorage(20)
+                .credentials(Credentials.fromGeneratedSecret("admin_user"))
+                .databaseName(dbName)
+                .removalPolicy(RemovalPolicy.DESTROY) // everytime we destroy the stack we want to destroy db
+                .build();
+    }
+
+    //Step 3: Creating health check for DB that is created above
+    private CfnHealthCheck createDbHealthCheck(DatabaseInstance db, String id){
+        return CfnHealthCheck.Builder.create(this, id)
+                .healthCheckConfig(CfnHealthCheck.HealthCheckConfigProperty.builder()
+                        .type("TCP")
+                        .port(Token.asNumber(db.getDbInstanceEndpointPort()))
+                        .ipAddress(db.getDbInstanceEndpointAddress())
+                        //checks health every 30 seconds
+                        .requestInterval(30)
+                        //tries 3 times before it reports failure
+                        .failureThreshold(3)
+                        .build())
                 .build();
     }
 
